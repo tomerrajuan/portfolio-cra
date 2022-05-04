c113c1b600ef9559beb3228a3586866ca40ceed0
tomer <tomer.rajuan@gmail.com>
2022-05-04 21:59:47 +0200

    remove react-bootstrap

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,49 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function Header() {
   return (
-    <div className="ocd-for-me-header">
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">Tomer Rajuan</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            {/* <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav> */}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-portfolio">
+      <a className="navbar-brand" href="#">
+        Tomer Rajuan
+        <br />
+        Frontend web developer
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#about">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Projects
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
