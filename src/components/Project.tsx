ee9e85ed5c4f5b4428f4952fb19771c7afe34206
tomer <tomer.rajuan@gmail.com>
2022-05-09 21:02:08 +0200

    [add-projects-section] add projects data file

---
+++ src/components/Project.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import ipipeImage from "images/ipipe.png";
+
+interface Props {
+  image: string;
+  title: string;
+  headline: string;
+  text: string;
+  link: string;
+  dataTarget: string;
+}
+export default function Project({
+  image,
+  title,
+  headline,
+  text,
+  link,
+  dataTarget,
+}: Props) {
+  return (
+    <>
+      <div className="projects-project">
+        <img className="projects-project__img" src={image} alt="" />
+
+        <div
+          className="projects-project__img-overlay"
+          data-toggle="modal"
+          data-target={"#" + dataTarget}
+        >
+          <i className="projects-project__img-overlay-icon fas fa-search-plus"></i>
+        </div>
+      </div>
+      <div
+        className="modal fade"
+        id={dataTarget}
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="projects-project-modal modal-dialog" role="document">
+          <div className="projects-project-modal-content modal-content">
+            <button
+              type="button"
+              className="projects-project-modal__close btn btn-secondary"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <h2
+              className="projects-project-modal__title"
+              id="exampleModalLabel"
+            >
+              {title}
+            </h2>
+
+            <div className="projects-project-modal-body modal-body">
+              <h3 className="projects-project-modal-body__headline">
+                {headline}
+              </h3>
+              <p className="projects-project-modal-body__text">{text}</p>
+            </div>
+            <div className="projects-project-modal-footer modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/data/projects.js
@@ -0,0 +1,62 @@
+import ipipe from "../assets/images/ipipe.png";
+import imageboard from "../assets/images/imageboard.png";
+import inter from "../assets/images/inter.jpg";
+import review from "../assets/images/review.png";
+import social from "../assets/images/social.png";
+import petition from "../assets/images/petition.png";
+
+export const projects = [{
+        type: "init",
+        image: ipipe,
+        title: "ipipe",
+        headline: "ipipe is a landing page for a start-up tech company from Israel.",
+        text: "Building that project was as one can belive, not about the HTML and CSS work but learning how to deploy and handle with the host services, working on daily bases with FileZilla and Bluehost, and exploring the statistics and user flow in the page. I was very glad to do this work and gain that experience from a very independent self taught work.",
+        link: "",
+        dataTarget: "project-1",
+    },
+    {
+        type: "init",
+        image: imageboard,
+        title: "",
+        headline: "",
+        text: "",
+        link: "",
+        dataTarget: "project-2",
+    },
+    {
+        type: "init",
+        image: inter,
+        title: "",
+        headline: "",
+        text: "",
+        link: "",
+        dataTarget: "project-3",
+    },
+    {
+        type: "private",
+        image: review,
+        title: "",
+        headline: "",
+        text: "",
+        link: "",
+        dataTarget: "project-4",
+    },
+    {
+        type: "private",
+        image: social,
+        title: "",
+        headline: "",
+        text: "",
+        link: "",
+        dataTarget: "project-5",
+    },
+    {
+        type: "private",
+        image: petition,
+        title: "",
+        headline: "",
+        text: "",
+        link: "",
+        dataTarget: "project-6",
+    },
+];
